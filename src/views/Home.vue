fa3b18f43e30aa495da6e240975a2e4c3b690940
wangyechao <wangyechao@yongliweb.com>
2019-08-16 17:54:10 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <cart></cart>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import cart from '@/components/Cart.vue' // @ is an alias to /src
+
+@Component({
+  components: {
+    cart
+  }
+})
+export default class Home extends Vue { }
+</script>
